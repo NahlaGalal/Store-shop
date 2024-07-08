1d08707bb9f5c3ce74aa3dcf94e10e20b2a6b70f
NahlaGalal <nahlaglal@gmail.com>
2024-07-09 02:36:30 +0300

    [UI] Add product card component template

---
+++ src/app/features/List/components/product-card/product-card.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { bootstrapStarFill } from '@ng-icons/bootstrap-icons';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [NgIconComponent, RouterLink],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.scss',
+  viewProviders: [provideIcons({ bootstrapStarFill })],
+})
+export class ProductCardComponent {}
