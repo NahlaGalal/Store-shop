ea7ec4f0acff36df517394d8bbf42365fc2bf2b4
NahlaGalal <nahlaglal@gmail.com>
2024-07-09 01:51:26 +0300

    [UI] Implement routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/List/home/home.component';
+import { SearchComponent } from './features/List/search/search.component';
+import { CategoryProductsComponent } from './features/List/category-products/category-products.component';
+import { ProductComponent } from './features/Details/product/product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'search/:handle',
+    component: SearchComponent,
+  },
+  {
+    path: 'categories/:handle',
+    component: CategoryProductsComponent,
+  },
+  {
+    path: 'product/:handle',
+    component: ProductComponent,
+  },
+  { path: '*', redirectTo: '', pathMatch: 'full' },
+];
