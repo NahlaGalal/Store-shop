a13306e6fd45397c145c19ae260a5f3cc276a2cc
NahlaGalal <nahlaglal@gmail.com>
2024-07-09 02:51:45 +0300

    [UI] Add layout of listing pages

--- src/app/features/List/category-products/category-products.component.ts
+++ src/app/features/List/category-products/category-products.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { BreadcrumbComponent } from '../../../shared/breadcrumb/breadcrumb.component';
+import { ProductCardComponent } from '../components/product-card/product-card.component';
+import { FilterComponent } from '../components/filter/filter.component';
 
 @Component({
   selector: 'app-category-products',
   standalone: true,
-  imports: [BreadcrumbComponent],
+  imports: [BreadcrumbComponent, FilterComponent, ProductCardComponent],
   templateUrl: './category-products.component.html',
   styleUrl: './category-products.component.scss'
 })
