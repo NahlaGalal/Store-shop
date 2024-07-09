065f25268118228a17f687cffd2f6ac0eb4ca233
NahlaGalal <nahlaglal@gmail.com>
2024-07-09 04:10:21 +0300

    [UI] Add price filter template

--- src/app/features/List/components/filter/filter.component.ts
+++ src/app/features/List/components/filter/filter.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CategoryFilterComponent } from '../category-filter/category-filter.component';
 import { BrandFilterComponent } from '../brand-filter/brand-filter.component';
+import { PriceFilterComponent } from '../price-filter/price-filter.component';
 
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [CategoryFilterComponent, BrandFilterComponent],
+  imports: [
+    CategoryFilterComponent,
+    BrandFilterComponent,
+    PriceFilterComponent,
+  ],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.scss',
 })
