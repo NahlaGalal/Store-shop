667d1999e7b3561ec981041f3730859954811dfa
NahlaGalal <nahlaglal@gmail.com>
2024-07-09 20:58:47 +0300

    [Integration] Navigate to error if the product id is invalid

--- src/app/features/Details/product/product.component.ts
+++ src/app/features/Details/product/product.component.ts
@@ -3,7 +3,7 @@ import { Component, inject } from '@angular/core';
 import { ProductInfoComponent } from '@/app/features/Details/components/product-info/product-info.component';
 import { ProductService } from '@/app/features/Details/services/product.service';
 import { Product } from '@/app/shared/product';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { LoadingService } from '@/app/core/loading.service';
 
 @Component({
@@ -17,6 +17,7 @@ export class ProductComponent {
   product: Product | undefined;
   private productService = inject(ProductService);
   private route = inject(ActivatedRoute);
+  private router = inject(Router);
   private loading = inject(LoadingService);
 
   ngOnInit(): void {
@@ -27,10 +28,13 @@ export class ProductComponent {
     this.productService.getProductDetails(id).subscribe({
       next: (data) => (this.product = data),
       error: (err) => {
-        console.log(err);
         this.loading.loadingOff();
+
+        if (err.status === 404) {
+          this.router.navigate(['/error'], { replaceUrl: true });
+        }
       },
-      complete: () => this.loading.loadingOff()
+      complete: () => this.loading.loadingOff(),
     });
   }
 }
