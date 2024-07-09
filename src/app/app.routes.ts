8ff6a1339023e4d6fda395303de58bcd74387038
NahlaGalal <nahlaglal@gmail.com>
2024-07-09 20:40:04 +0300

    [UI] Error page template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,23 +3,32 @@ import { HomeComponent } from './features/List/home/home.component';
 import { SearchComponent } from './features/List/search/search.component';
 import { CategoryProductsComponent } from './features/List/category-products/category-products.component';
 import { ProductComponent } from './features/Details/product/product.component';
+import { NotFoundComponent } from './features/Errors/not-found/not-found.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    title: "Home - Convertedin Store shop",
   },
   {
     path: 'search/:handle',
     component: SearchComponent,
+    title: "Search - Convertedin Store shop",
   },
   {
     path: 'categories/:handle',
     component: CategoryProductsComponent,
+    title: "Categories - Convertedin Store shop",
   },
   {
-    path: 'product/:handle',
+    path: 'product/:id',
     component: ProductComponent,
+    title: "Product - Convertedin Store shop",
   },
-  { path: '*', redirectTo: '', pathMatch: 'full' },
+  {
+    path: '**',
+    component: NotFoundComponent,
+    title: "Not found - Convertedin Store shop",
+  },
 ];
